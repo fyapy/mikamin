b0ffc50af73d86557b3938ded3584292b79bfec2
Amir Abdullin <amirop35@gmail.com>
2023-08-09 12:39:02 +0300

    feature add boolStr rule and rename numeric to numberStr

--- src/rules/bool.ts
+++ src/rules/bool.ts
@@ -6,3 +6,9 @@ export const bool: Rule = {
   errorMessage: translations.bool,
   valid: value => typeof value === 'boolean',
 }
+
+export const boolStr: Rule = {
+  rule: 'bool',
+  errorMessage: translations.bool,
+  valid: value => value === 'true' || value === 'false',
+}
--- src/rules/number.ts
+++ src/rules/number.ts
@@ -6,3 +6,18 @@ export const number: Rule = {
   errorMessage: translations.number,
   valid: value => typeof value === 'number',
 }
+
+export const numberStr: Rule = {
+  rule: 'number',
+  errorMessage: translations.number,
+  valid: value => {
+    if (value === null) {
+      return false
+    }
+    if (typeof value === 'string' && value.length === 0) {
+      return false
+    }
+
+    return !isNaN(Number(value))
+  },
+}
