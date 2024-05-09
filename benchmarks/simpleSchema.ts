27a03c45ea0c31219884cadf726e4485f2061d5f
fyapy <amirop35@gmail.com>
2024-05-09 14:42:17 +0300

    chore add yup, zod and joi benchmarks

--- benchmarks/simpleSchema.ts
+++ benchmarks/simpleSchema.ts
@@ -1,8 +1,37 @@
+import {z} from 'zod'
+import joi from 'joi'
 import {d} from 'desy'
+import * as yup from 'yup'
 import {bench, group} from 'mitata'
 import {stringNumber, handleSchema} from '../src/index.js'
 
 group('simple schema', () => {
+  const joiSimpleSchema = joi.object({
+    from: joi.number().required(),
+  })
+
+  bench('joi', () => {
+    joiSimpleSchema.validate({})
+  })
+
+  const yupSimpleSchema = yup.object({
+    from: yup.number().required(),
+  })
+
+  bench('yup', () => {
+    try {
+      yupSimpleSchema.validateSync({})
+    } catch {}
+  })
+
+  const zodSimpleSchema = z.object({
+    from: z.number().int(),
+  })
+
+  bench('zod', () => {
+    zodSimpleSchema.safeParse({})
+  })
+
   const desySimpleSchema = d.object({
     from: d.number().int(),
   })
--- benchmarks/simpleSchemaInit.ts
+++ benchmarks/simpleSchemaInit.ts
@@ -1,21 +1,50 @@
+import {z} from 'zod'
+import joi from 'joi'
 import {d} from 'desy'
+import * as yup from 'yup'
 import {bench, group} from 'mitata'
 import {handleSchema, stringNumber} from '../src/index.js'
 
 group('simple schema with init', () => {
+  bench('joi', () => {
+    const schema = joi.object({
+      from: joi.number().required(),
+    })
+
+    schema.validate({})
+  })
+
+  bench('yup', () => {
+    const schema = yup.object({
+      from: yup.number().required(),
+    })
+
+    try {
+      schema.validateSync({})
+    } catch {}
+  })
+
+  bench('zod', () => {
+    const schema = z.object({
+      from: z.number().int(),
+    })
+
+    schema.safeParse({})
+  })
+
   bench('desy', () => {
     const schema = d.object({
       from: d.number().int(),
     })
-  
+
     schema.validate({})
   })
-  
+
   bench('mikamin', () => {
-    const mikaminSimpleSchema = {
+    const schema = {
       from: stringNumber,
     }
-  
-    handleSchema({schema: mikaminSimpleSchema, values: {}})
+
+    handleSchema({schema, values: {}})
   })
 })
